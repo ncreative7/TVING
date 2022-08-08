602184025f676728156516b0ff72a1bce2bf9c10
ncreative7 <ncreative7@naver.com>
2022-08-08 12:39:15 +0900

    second

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,60 @@
 $(document).ready(function () {
 
-  //대댓글 토글 view_material---------------------------------------------
-  $('.reply_btn').click(function(){
-    $('#reply_wrap').toggle();
+  //메인 배너 슬릭 슬라이드
+  $('.main_ban_slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    pauseOnHover:false, //마우스 올려도 계속 넘ㅋ김
   })
+
+  //메인 배너 슬릭 슬라이드 재생/정지 버튼
+  $('.btn_pause').click(function(){
+    $('.main_ban_slider').slick('slickPause');
+    $('.btn_pause').removeClass('active');
+    $('.btn_play').addClass('active');
+  })
+
+  $('.btn_play').click(function(){
+	  $('.main_ban_slider').slick('slickPlay');
+    $('.btn_play').removeClass('active');
+    $('.btn_pause').addClass('active');
+  });
+
+
+  //swiper : 영상 컨텐츠 슬라이드
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 7, //한 슬라이드에 보여줄 갯수
+    spaceBetween: 30, //슬라이드 사이 여백
+    loop: true, //슬라이드 반복
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+   $('.mypage').mouseover(function() {
+   $("#mypage_wrap").css("display", "block");
+  });
+  $('.mypage').mouseout(function() {
+   $("#mypage_wrap").css("display", "none");
+  });
+
+  $('#mypage_wrap').mouseover(function() {
+   $("#mypage_wrap").css("display", "block");
+  });
+  $('#mypage_wrap').mouseout(function() {
+   $("#mypage_wrap").css("display", "none");
+  });
+
+
+  //검색창 토글 --------------------------------------------
+  // $('.search_bt').click(function(){
+  //   $('#search_wrap').toggle();
+  // })
   
   //상단 올라가는 버튼
   $('#top_btn').click(function () {
@@ -11,24 +62,5 @@ $(document).ready(function () {
       'scrollTop': 0,
     }, 1500)
   })
-
-  //메인 배너 슬릭 슬라이드
-  $('.main_ban_slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-  })
-
-  /*swiper*/
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 7, //한 슬라이드에 보여줄 갯수
-  spaceBetween: 30, //슬라이드 사이 여백
-  loop: true, //슬라이드 반복
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 
 })
